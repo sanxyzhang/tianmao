9d96c246d9e24f9d288dcc76006d0e798bdd904c
sanxyzhang <852667805@qq.com>
2018-06-18 16:17:18 +0800

    otherpage

--- src/app/categorywithcarousel/categorywithcarousel.component.ts
+++ src/app/categorywithcarousel/categorywithcarousel.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorywithcarouselComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
